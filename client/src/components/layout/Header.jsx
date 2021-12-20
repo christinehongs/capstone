816e14c1d6cfca7114017012d20ca986dddd487c
Sam Davis <hsndm16@yahoo.com>
2021-12-20 14:59:00 -0600

    test2

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -26,7 +26,6 @@ export default function Nav() {
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          {/* dkjfgbdfg */}
           <HStack spacing='1rem'>
             <Link to='/'>Home Page</Link>
             <Link to='/sample'>Redux Button</Link>
