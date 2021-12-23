969ea798af22319bb2c6deaa26714d6714994389
Sam Davis <hsndm16@yahoo.com>
2021-12-23 10:03:20 -0600

    finished conversion chart

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -26,9 +26,12 @@ export default function Nav() {
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          
           <HStack spacing='1rem'>
             <Link to='/'>Home Page</Link>
             <Link to='/sample'>Redux Button</Link>
+            <Link to='/converter'>Currency Converter</Link>
+            
           </HStack>
 
           <Flex alignItems={"center"}>
