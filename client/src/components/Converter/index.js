1916b2b927e3db4596123f672f0c9839725dd808
Emily Ye <emily.ye.dev@gmail.com>
2022-01-05 04:04:47 -0500

    changes label, refactors converter props

--- client/src/components/Converter/index.js
+++ client/src/components/Converter/index.js
@@ -6,14 +6,14 @@ function Converter(props) {
     <Stack alignItems={'center'}>
       <div>
         {props.fromAmount} {props.firstInput} <h1>=</h1>
-      <Text>
-        {props.toAmount} {props.secondInput}
-      </Text>
+        <Text>
+          {props.toAmount} {props.secondInput}
+        </Text>
       </div>
-      <HStack sx={{ marginTop: '1.5rem !important'}}>
+      <HStack sx={{ marginTop: '1.5rem !important' }}>
         <Input
           type="number"
-          value={props.fromAmount >= 1000000000 ? 1000000000: props.fromAmount}
+          value={props.fromAmount >= 1000000000 ? 1000000000 : props.fromAmount}
           onChange={props.onMoneyChangeFrom}
           min="1"
           width={'10rem'}
@@ -32,28 +32,28 @@ function Converter(props) {
           })}
         </Select>
       </HStack>
-      <HStack>
-        <Input
-          type="number"
-          value={props.toAmount}
-          onChange={props.onMoneyChangeTo}
-          min="1"
-          width={'10rem'}
-        />
-        <Select
-          value={props.secondInput}
-          onChange={props.handleToCurrency}
-          width={'6rem'}
-        >
-          {props.data.map((rate) => {
-            return (
-              <option key={rate} value={rate}>
-                {rate}
-              </option>
-            );
-          })}
-        </Select>
-      </HStack>
+      {/*<HStack>*/}
+      {/*  <Input*/}
+      {/*    type="number"*/}
+      {/*    value={props.toAmount}*/}
+      {/*    onChange={props.onMoneyChangeTo}*/}
+      {/*    min="1"*/}
+      {/*    width={'10rem'}*/}
+      {/*  />*/}
+      {/*  <Select*/}
+      {/*    value={props.secondInput}*/}
+      {/*    onChange={props.handleToCurrency}*/}
+      {/*    width={'6rem'}*/}
+      {/*  >*/}
+      {/*    {props.data.map((rate) => {*/}
+      {/*      return (*/}
+      {/*        <option key={rate} value={rate}>*/}
+      {/*          {rate}*/}
+      {/*        </option>*/}
+      {/*      );*/}
+      {/*    })}*/}
+      {/*  </Select>*/}
+      {/*</HStack>*/}
     </Stack>
   );
 }
