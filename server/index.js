e22fe23f035b93e12d9edad9111f9caf14dbec92
Emily Ye <emily.ye.dev@gmail.com>
2022-01-05 05:06:59 -0500

    successfully retrieves items in items collection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
-import { connectDb } from './config/mongoose';
+// import { connectDb } from './config/mongoose';
+import { MongoClient } from 'mongodb';
 import citiesRouter from './routes/cities';
 import itemsRouter from './routes/items';
 import cartRouter from './routes/items';
@@ -8,7 +9,53 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-connectDb();
+const uriEndpoint = new MongoClient(process.env.MONGODB_URI_ENDPOINT);
+// const database = uriEndpoint.db('capstone');
+// const items = database.collection('items');
+
+async function findOneListingByName(client, nameOfListing) {
+  const result = await client
+    .db('capstone')
+    .collection('items')
+    .findOne({ name: nameOfListing });
+  if (result) {
+    console.log(
+      `Found a listing in the collection with the name '${nameOfListing}':`
+    );
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+async function findItems(client) {
+  const result = await client
+    .db('capstone')
+    .collection('items')
+    .find({})
+    // .limit()
+    .toArray();
+  if (result) {
+    console
+      .log
+      // `Found a listing in the collection with the name '${nameOfListing}':`
+      ();
+    console.log(result);
+  } else {
+    // console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+MongoClient.connect(uriEndpoint, async () => {
+  try {
+    await uriEndpoint.connect();
+    // await findOneListingByName(uriEndpoint, 'apples');
+    await findItems(uriEndpoint);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
 const app = express();
 
 app.use(express.static('public'));
--- server/routes/cities.js
+++ server/routes/cities.js
@@ -1,6 +1,9 @@
 import lodash from 'lodash';
 import express from 'express';
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const client = new MongoClient(process.env.MONGODB_URI_ENDPOINT);
 
@@ -9,18 +12,42 @@ const items = database.collection('items');
 
 const citiesRouter = express.Router();
 
-citiesRouter.get('/cities', async (_, res) => {
-  const allItems = await items.find();
-  const citiesWithDups = allItems.map((item) => item.city);
+MongoClient.connect(client, async () => {
+  // assert.equal(null, err);
+  try {
+    await client.connect();
 
-  const allCities = lodash
-    .chain(citiesWithDups)
-    .uniq()
-    .orderBy((city) => city.toLowerCase())
-    .value();
+    // await listDatabases(client);
 
-  console.log(allCities.length);
-  res.send(allCities);
+    const allItems = await items.find();
+    // console.log(allItems.length);
+    const citiesWithDups = allItems.map((item) => item.city);
+
+    const allCities = lodash
+      .chain(citiesWithDups)
+      .uniq()
+      .orderBy((city) => city.toLowerCase())
+      .value();
+
+    // console.log(allCities.length);
+    // res.send(allCities);
+  } catch (e) {
+    console.error(e);
+  }
 });
+
+// citiesRouter.get('/cities', async (_, res) => {
+//   const allItems = await items.find();
+//   const citiesWithDups = allItems.map((item) => item.city);
+//
+//   const allCities = lodash
+//     .chain(citiesWithDups)
+//     ._uniq()
+//     .orderBy((city) => city.toLowerCase())
+//     .value();
+//
+//   console.log(allCities.length);
+//   res.send(allCities);
+// });
 
 export default citiesRouter;
